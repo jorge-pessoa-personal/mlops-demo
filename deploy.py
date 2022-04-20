e1e4f50022b740c2dc7457db9fa610bcadc035f7
Jorge Pessoa <jorge@ntropy.network>
2022-04-20 02:50:08 +0100

    Fix deploy

--- deploy.py
+++ deploy.py
@@ -1,15 +1,18 @@
 import os
 import neptune.new as neptune
+import uuid
+
+HASH = uuid.uuid4().hex[:5]
 
 neptune.init_model(
-    name="production-model",
+    name=f"production-model-{HASH}",
     project="jpessoa/demo", 
-    key="PROD",
+    key=f"PROD_{HASH}",
     api_token=os.environ["NEPTUNE_API_KEY"]
 )
 
 model = neptune.init_model_version(
-    model="DEM-PROD",
+    model="DEM-PROD_{HASH}",
     project="jpessoa/demo", 
     api_token=os.environ["NEPTUNE_API_KEY"]
 )
